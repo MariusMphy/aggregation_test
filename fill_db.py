f1bef666eb300c272f12d2c08916f686a045ed75
MariusMphy <marius.markelis@gmail.com>
2024-04-14 10:42:19 +0300

    fill_db updated ending date input, created folder for libraries

--- fill_db.py
+++ fill_db.py
@@ -9,7 +9,7 @@ class RandDB:
 
     @staticmethod
     def randomize_word(library):
-        with open(f'{library}.txt', 'r') as file:
+        with open(f'libraries\\{library}.txt', 'r') as file:
             # Read the contents of the file
             contents = file.read()
         # Split the text into individual words
@@ -43,21 +43,24 @@ class RandDB:
         # str_fields_no = int(input("Enter how many string fields you want: "))
         str_field1_library = input("Chose string field 1 from (animals5, animals20, cars5, cars20, "
                                    "electronics5, electronics20, letters5, letters20, names5, names20, names200"
-                                   "words5, words20, words213)")
+                                   "words5, words20, words213): ")
         str_field1 = input("Enter name for string field 1: ")
         str_field2_library = input("Chose string field 1 from (animals5, animals20, cars5, cars20, "
                                    "electronics5, electronics20, letters5, letters20, names5, names20, names200"
-                                   "words5, words20, words213)")
+                                   "words5, words20, words213): ")
         str_field2 = input("Enter name of string field 2: ")
         str_date = input("Enter date field name: ")
         starting_date = input("Enter starting date for date field (yyyy-mm-dd): ")
+        ending_date = input("Enter ending date for date field (yyyy-mm-dd) or press enter for today: ")
+        str_today = datetime.today().strftime("%Y-%m-%d")
+        ending_confirmed = str_today if not ending_date else ending_date
         int_field = input("Enter name of integer field: ")
         int_start = int(input("Enter starting number of integer field range: "))
         int_end = int(input("Enter ending number of integer field range: "))
         float_field = input("Enter name of float field: ")
         float_start = round(float(input("Enter starting number of float field range: ")), 4)
         float_end = round(float(input("Enter ending number of float field range: ")), 4)
-        str_today = datetime.today().strftime("%Y-%m-%d")
+
         client = MongoClient("localhost", 27017)
         db = client[db_name]
         collection = db[collection_name]
@@ -65,7 +68,7 @@ class RandDB:
             goods = {
                 str_field1: self.randomize_word(str_field1_library),
                 str_field2: self.randomize_word(str_field2_library),
-                str_date: self.random_date(starting_date, str_today),
+                str_date: self.random_date(starting_date, ending_confirmed),
                 int_field: random.randint(int_start, int_end),
                 float_field: round(random.uniform(float_start, float_end), 4)
             }
